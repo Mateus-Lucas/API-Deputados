3ba1d5e297d65e501382340d0a194e71a0a1e5ab
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-04-04 13:23:54 -0300

    Adicionando Cabeçalho e Rodapé

---
+++ trabalho/componentes/Pagina0.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Cabecalho from './Cabecalho'
+import Rodape from './Rodape'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Pagina0 = () => {
+  return (
+    <>
+     <Cabecalho/>
+     <Rodape/>
+    </>
+  )
+}
+
+export default Pagina0
