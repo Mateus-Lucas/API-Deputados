6fa94820bee739939b308f5162c2a91a5f2d7948
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-04-07 17:48:29 -0300

    criando Cabecalho/Rodape

--- trabalho/componentes/Cabecalho.js
+++ trabalho/componentes/Cabecalho.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Nav } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Cabecalho = () => {
   return (
-    <div>Cabecalho</div>
-  )
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#">
+            <img
+              alt=""
+              src="https://www.desterrodomelo.mg.leg.br/imagens/brasao-republica.png/image_preview"
+              width="40"
+              height="40"
+              className="d-inline-block align-top"
+            />{' '}
+            Câmara dos Deputados do Brasil
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Início</Nav.Link>
+            <Nav.Link href="#features">Sobre</Nav.Link>
+            <Nav.Link href="#pricing">Projetos</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default Cabecalho
--- trabalho/componentes/Rodape.js
+++ trabalho/componentes/Rodape.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Container, Navbar } from 'react-bootstrap'
 
 const Rodape = () => {
   return (
-    <div>Rodape</div>
+ 
+  <div className='bg-primary text-white text-center position-fixed bottom-0' style={{width: '100%', padding: '10px'}}>
+    <p>© 2023 Câmara dos Deputados do Brasil. Todos os direitos reservados.</p>
+  </div>
   )
 }
 
