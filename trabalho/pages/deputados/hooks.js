6c7e0cc7ca0d2cc5d3dd777c09db03cf58ef003a
YuriSFG <yurisfg@gmail.com>
2023-04-11 10:06:39 -0300

    Atualização da pasta de conteudo servc api e hooks

---
+++ trabalho/pages/deputados/hooks.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const hooks = () => {
+    let nome = ''
+    function alterarNome() {
+        nome = ''
+    }
+    return (
+        <div>hooks</div>
+    )
+}
+
+export default hooks
