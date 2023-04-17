f79eee28f7b77f35c3723f72e53d5d6937805501
YuriSFG <yurisfg@gmail.com>
2023-04-17 16:20:03 -0300

    Adicionando informações dos deputados
    Partidos siglas e nome dos deputados

--- trabalho/pages/deputados/hooks.js
+++ trabalho/pages/deputados/hooks.js
@@ -5,22 +5,33 @@ import apiDeputados from '../../services/apiDeputados';
 const hooks = () => {
 
     const [deputados, setDeputados] = useState([])
-  
-    useEffect(()=>{
-  
-        apiDeputados.get('/deputados').then(resultado=>{
+
+    useEffect(() => {
+
+        apiDeputados.get('/deputados').then(resultado => {
             setDeputados(resultado.data.dados)
         })
-            
+
     }, [])
-  
+
     return (
         <>
+
             <h1>Nome dos Deputados</h1>
             <br></br>
+
+
             {deputados.map(item => (
-                <img title={item.nome} key={item.id} src={item.urlFoto} width='150px' />
+                <img title={item.nome} key={item.id} src={item.urlFoto} title2={item.siglaUf} width='150px' />
             ))}
+
+            {deputados.map(item => (
+                <p>{item.siglaUf}</p>
+            ))}
+               {deputados.map(item => (
+                <p>{item.nome}</p>
+            ))}
+            
         </>
     )
 }
