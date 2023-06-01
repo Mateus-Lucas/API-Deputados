df7ee0d396bd12c47613d207bcd3e81943947855
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-06-01 16:02:04 -0300

    otimização do Calendário

--- trabalho/pages/deputados/[id].js
+++ trabalho/pages/deputados/[id].js
@@ -1,22 +1,36 @@
-import React, { useState } from 'react'
-import { Button, Card, Col, Row, Table } from 'react-bootstrap'
-import apiDeputados from '../../services/apiDeputados'
-import Pagina from '../../components/Pagina'
-import Link from 'next/link'
+import React, { useEffect, useState } from 'react';
+import { Button, Card, Col, Row, Table } from 'react-bootstrap';
+import apiDeputados from '../../services/apiDeputados';
+import Pagina from '../../components/Pagina';
+import Link from 'next/link';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 
 const Detalhes = ({ deputado, despesas, profissoes }) => {
+  const [somaDespesas, setSomaDespesas] = useState(0);
+  const [eventosDespesas, setEventosDespesas] = useState([]);
+  const [calendarioVisivel, setCalendarioVisivel] = useState(false);
 
-  const [somaDespesas, setSomaDespesas] = useState(0); // Estado para armazenar a soma das despesas
-
-  // Função para calcular a soma das despesas
   const calcularSomaDespesas = () => {
     const soma = despesas.reduce((acumulador, item) => acumulador + item.valorDocumento, 0);
     setSomaDespesas(soma);
   };
 
-  // Função para redefinir o valor da soma das despesas para zero
   const resetSomaDespesas = () => {
     setSomaDespesas(0);
+  };
+
+  useEffect(() => {
+    const eventos = despesas.map((item) => ({
+      title: item.tipoDespesa,
+      date: item.dataDocumento
+    }));
+
+    setEventosDespesas(eventos);
+  }, [despesas]);
+
+  const toggleCalendario = () => {
+    setCalendarioVisivel(!calendarioVisivel);
   };
 
   return (
@@ -37,57 +51,42 @@ const Detalhes = ({ deputado, despesas, profissoes }) => {
         </Col>
         <Col md={7}>
           <h1>Despesas</h1>
-          <Table striped bordered hover size="sm">
-            <thead>
-              <tr>
-                <th>Data</th>
-                <th>Descrição</th>
-                <th>Valor</th>
-              </tr>
-            </thead>
-            <tbody>
-              {despesas.map((item) => (
-                <tr key={item.id}>
-                  <td>{item.dataDocumento}</td>
-                  <td>{item.tipoDespesa}</td>
-                  <td>{item.valorDocumento}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-          <Button variant='primary' onClick={calcularSomaDespesas}>Somar Despesas</Button> {/* Botão para calcular a soma das despesas */}
-          <Button variant='danger' onClick={resetSomaDespesas}>Reset</Button> {/* Botão para redefinir a soma das despesas */}
-          <p>Total das despesas: R${somaDespesas}</p> {/* Exibe a soma das despesas */}
-
+          <Button variant='secondary' onClick={toggleCalendario}>
+            {calendarioVisivel ? 'Ocultar Calendário' : 'Exibir Calendário'}
+          </Button>
+          {calendarioVisivel && (
+            <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" events={eventosDespesas} />
+          )}
+          <Button variant='primary' onClick={calcularSomaDespesas}>Somar Despesas</Button>
+          <Button variant='danger' onClick={resetSomaDespesas}>Reset</Button>
+          <p>Total das despesas: R${somaDespesas}</p>
         </Col>
-        <Col md={2}>
-          <h1>Profissões</h1>
-          <ul>
-            {profissoes.map((item, index) => (
-              <li key={index}>{item.titulo}</li>
-            ))}
-          </ul>
+        <Col>
+          {profissoes.map((item) =>(
+            <ul><li>{item.titulo}</li></ul>
+          ))}
         </Col>
       </Row>
     </Pagina>
-  )
-}
+  );
+};
 
-export default Detalhes
+export default Detalhes;
+
 
 export async function getServerSideProps(context) {
-  const id = context.params.id
+  const id = context.params.id;
 
-  const dep = await apiDeputados.get('/deputados/' + id)
-  const deputado = dep.data.dados
+  const dep = await apiDeputados.get('/deputados/' + id);
+  const deputado = dep.data.dados;
 
-  const desp = await apiDeputados.get('/deputados/' + id + '/despesas')
-  const despesas = desp.data.dados
+  const desp = await apiDeputados.get('/deputados/' + id + '/despesas');
+  const despesas = desp.data.dados;
 
-  const prof = await apiDeputados.get('/deputados/' + id + '/profissoes')
-  const profissoes = prof.data.dados
+  const prof = await apiDeputados.get('/deputados/' + id + '/profissoes');
+  const profissoes = prof.data.dados;
 
   return {
-    props: { despesas, deputado, profissoes },
-  }
+    props: { despesas, deputado, profissoes }
+  };
 }
--- trabalho/pages/votacoes/index.js
+++ trabalho/pages/votacoes/index.js
@@ -4,6 +4,7 @@ import apiDeputados from '../../services/apiDeputados'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
+
 const index = ({ votacoes }) => {
 
   return (
@@ -13,8 +14,8 @@ const index = ({ votacoes }) => {
         initialView="dayGridMonth"
         weekends={false}
         events={[
-          { title: 'event 1', date: '2019-04-01' },
-          { title: 'event 2', date: '2019-04-02' }
+          { title: 'event 1', date: '2023-05-01' },
+          { title: 'event 2', date: '2023-05-01' }
         ]}/>
     </Pagina>
   )
