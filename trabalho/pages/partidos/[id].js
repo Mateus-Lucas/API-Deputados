a0b8cff9a89bd4f245932a69365ba7d42968b6d1
YURI SALES FERREIRA GONCALVES <128000462+YuriSFG@users.noreply.github.com>
2023-06-18 16:56:23 -0300

    Update [id].js
    Atualização da página id partido (Yuri)

--- trabalho/pages/partidos/[id].js
+++ trabalho/pages/partidos/[id].js
@@ -3,7 +3,7 @@ import apiDeputados from '@/services/apiDeputados';
 import React, { useState, useEffect } from 'react';
 import { Button, Card, Col, Row, Table } from 'react-bootstrap';
 
-const Detalhes = ({ partido, lideres}) => {
+const Detalhes = ({ partido, lideres }) => {
   const [scrollTop, setScrollTop] = useState(0);
 
   useEffect(() => {
@@ -32,30 +32,30 @@ const Detalhes = ({ partido, lideres}) => {
         </Col>
       </Row>
 
-      <Row md={4} className="justify-content-center">
-        {lideres.map((lider) => (
-          <Card key={lider.id}>
-            <Card.Img
-              variant="top"
-              alt=""
-              src={lider.urlFoto}
-              width="100"
-              height="300"
-            />
-            <Card.Body>
-              <Card.Title>{lider.nome}</Card.Title>
-            </Card.Body>
-          </Card>
-        ))}
+      <Row className="justify-content-center">
+        <Col md={12}>
+          <h3>Líder Partidário:</h3>
+          <div className="d-flex flex-wrap justify-content-center">
+            {lideres.map((lider) => (
+              <Card key={lider.id} className="m-3">
+                <Card.Img variant="top" alt="" src={lider.urlFoto} width="100" height="300" />
+                <Card.Body>
+                  <Card.Title>{lider.nome}</Card.Title>
+                  <Card.Text>{lider.cargo}</Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
+        </Col>
       </Row>
 
       <Table striped bordered hover className="mt-4">
         <thead>
           <tr>
-            <th>ordem</th>
+            <th>Ordem</th>
             <th>Nome</th>
             <th>Email</th>
-            <th>uf</th>
+            <th>UF</th>
             <th>Data Inicio</th>
             <th>Cargo</th>
           </tr>
@@ -75,14 +75,9 @@ const Detalhes = ({ partido, lideres}) => {
       </Table>
 
       {scrollTop > 0 && (
-        <Button
-          variant="primary"
-          className="scroll-to-top-btn"
-          onClick={handleScrollToTop}
-          GiSergeant
-          >
+        <Button variant="primary" className="scroll-to-top-btn" onClick={handleScrollToTop}>
           Voltar ao topo
-        </Button >
+        </Button>
       )}
     </Pagina>
   );
@@ -99,8 +94,7 @@ export async function getServerSideProps(context) {
   const partilider = await apiDeputados.get('/partidos/' + id + '/lideres');
   const lideres = partilider.data.dados;
 
-  
   return {
-    props: { partido, lideres,},
+    props: { partido, lideres },
   };
 }
