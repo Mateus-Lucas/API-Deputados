f2c461be7fc6b35d6b41386a1d1b14d0b615426e
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-06-19 13:53:42 -0300

    Atualização da pagina de proposicoes

--- trabalho/pages/proposicoes/index.js
+++ trabalho/pages/proposicoes/index.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Pagina from '../../components/Pagina';
 import apiDeputados from '../../services/apiDeputados';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import { Card, Col, Row } from 'react-bootstrap';
+import { Card, Col, DropdownButton, Row, Dropdown } from 'react-bootstrap';
 import Link from 'next/link';
 
-const Index = ({ proposicoes }) => {
+const Index = ({ deputados }) => {
+
+
+  const [proposicoesDeputado, setProposicoesDeputados] = useState([])
+  const [nomeDeputado, setNomeDeputado] = useState()
+
+
+  async function filtrarProposicao(id) {
+    const response = await apiDeputados.get(`/proposicoes?idDeputadoAutor=${id}&ano=2023&itens=15`);
+    const response2 = await apiDeputados.get(`/deputados/${id}`);
+    setNomeDeputado(response2.data.dados.ultimoStatus.nome)
+    setProposicoesDeputados(response.data.dados)
+
+  }
+
+
+
+
   return (
-    <Pagina titulo="Proposições">
+    <Pagina titulo="Proposições" >
       <Row md={6}>
-        {proposicoes.map((proposicao) => (
+        <DropdownButton
+          variant="primary"
+          title="Escolha o deputado"
+          onSelect={filtrarProposicao}
+          style={{ overflowY: 'scroll', height: '200px' }}
+        >
+          {deputados.map(item => (
+            <Dropdown.Item eventKey={item.id} key={item.id}> {item.nome} </Dropdown.Item>
+          ))}
+        </DropdownButton>
+        <Col>
+          <h3>Deputado: {nomeDeputado}</h3>
+        </Col>
+        {proposicoesDeputado.map((proposicao) => (
           <Col key={proposicao.id}>
-            <Card className="mb-4">
+            <Card className="mb-4" style={{ height: '18rem' }}>
               <Link href={`/proposicoes/${proposicao.id}`}>
                 <Card.Img variant="top" src={proposicao.urlImagem} />
                 <Card.Body>
-                <Card.Text>{proposicao.id}</Card.Text>
-                  <Card.Text>{proposicao.ementa}</Card.Text>
+                  <Card.Text>{proposicao.id}</Card.Text>
+                  <Card.Text>{proposicao.ementa.substring(0, 100)}</Card.Text>
                 </Card.Body>
               </Link>
             </Card>
@@ -26,21 +55,25 @@ const Index = ({ proposicoes }) => {
     </Pagina>
   );
 };
-
 export default Index;
 
 export async function getServerSideProps(context) {
   try {
-    const resultado = await apiDeputados.get('/proposicoes?ano=2023&itens=50');
-    const proposicoes = resultado.data.dados;
+    const response = await apiDeputados.get('/deputados');
+    const deputados = response.data.dados;
 
     return {
-      props: { proposicoes },
+      props: { deputados },
     };
+
   } catch (error) {
+
     console.error('Erro ao obter votações:', error);
+
     return {
       props: { proposicoes: [] },
     };
+
   }
+
 }
