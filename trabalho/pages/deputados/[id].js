71afccc11376696bbcdcfa4cd2be1627dcd4dff2
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-06-26 23:58:10 -0300

    Ultima correção de erro key-id-deputados

--- trabalho/pages/deputados/[id].js
+++ trabalho/pages/deputados/[id].js
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 
-const Detalhes = ({ deputado, profissoes }) => {
+const Detalhes = ({ deputado, profissoes, discursos }) => {
   const [somaDespesas, setSomaDespesas] = useState(0);
   const [eventosDespesas, setEventosDespesas] = useState([]);
   const [calendarioVisivel, setCalendarioVisivel] = useState(false);
@@ -64,22 +64,43 @@ const Detalhes = ({ deputado, profissoes }) => {
           <Card className='mb-4'>
             <Card.Img variant="top" key={deputado.id} src={deputado.ultimoStatus.urlFoto} />
             <Card.Body>
-              <Card.Title>{deputado.ultimoStatus.nome}</Card.Title>
-              <Card.Text>Partido: {deputado.ultimoStatus.siglaPartido}</Card.Text>
-              <Card.Text>UF Partido: {deputado.ultimoStatus.siglaUf}</Card.Text>
+              <Card.Title><strong>{deputado.ultimoStatus.nome}</strong></Card.Title>
+              <Card.Text><strong>Partido:</strong> {deputado.ultimoStatus.siglaPartido}</Card.Text>
+              <Card.Text><strong>UF Partido:</strong> {deputado.ultimoStatus.siglaUf}</Card.Text>
             </Card.Body>
           </Card>
           <Link href='/deputados/'>
             <Button variant='danger' size='sm'>Voltar</Button>
           </Link>
         </Col>
-        <Col>
-          <h1>Profissões</h1>
-          {profissoes.map((item) => (
-            <ul key={item.id}>
-              <li key={item.id}>{item.titulo}</li>
-            </ul>
-          ))}
+        <Col className='text-justify' md={6}>
+          <h4>Discurso</h4>
+          {discursos.length > 0 ? (
+            <p className='text-justify' dangerouslySetInnerHTML={{ __html: discursos[0].transcricao }}></p>
+          ) : (
+            <p>Nenhum discurso encontrado</p>
+          )}
+        </Col>
+        <Col md={3}>
+          <h4>Profissões</h4>
+          <ul>
+            {profissoes.map((item, index) => (
+              <li key={`profissao_${index}_${item.id}`}>{item.titulo}</li>
+            ))}
+          </ul>
+
+          <h4>Escolaridade</h4>
+          <ul>
+            <li key="escolaridade">{deputado.escolaridade}</li>
+          </ul>
+          <h4>Condição Eleitoral</h4>
+          <ul>
+            <li key="condicaoEleitoral">{deputado.ultimoStatus.condicaoEleitoral}</li>
+          </ul>
+          <h4>Situação Atual</h4>
+          <ul>
+            <li key="situacaoAtual">{deputado.ultimoStatus.situacao}</li>
+          </ul>
         </Col>
       </Row>
       <br></br>
@@ -133,13 +154,20 @@ export default Detalhes;
 export async function getServerSideProps(context) {
   const id = context.params.id;
 
-  const dep = await apiDeputados.get('/deputados/' + id);
-  const deputado = dep.data.dados;
+  const depResp = await apiDeputados.get(`/deputados/${id}`);
+  const deputado = depResp.data.dados;
 
-  const prof = await apiDeputados.get('/deputados/' + id + '/profissoes');
-  const profissoes = prof.data.dados;
+  const profResp = await apiDeputados.get(`/deputados/${id}/profissoes`);
+  const profissoes = profResp.data.dados;
+
+  const discResp = await apiDeputados.get(`/deputados/${id}/discursos`);
+  const discursos = discResp.data.dados;
 
   return {
-    props: { deputado, profissoes }
+    props: {
+      deputado,
+      profissoes,
+      discursos,
+    },
   };
 }
